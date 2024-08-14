008a69ec95d6dfd59a2c22c35cefd6e4042a26ac
Gustavo Silva <126930154+GustavoSilvadeCarvalho@users.noreply.github.com>
2024-08-14 08:22:43 -0300

    aula-6
    Criando o crud

---
+++ script-crud.js
@@ -0,0 +1,20 @@
+// encontrar o botão adicionar tarefa
+
+const btnAdicionarTarefa = document.querySelector('.app__button--add-task')
+const formAdicionarTarefa = document.querySelector('.app__form-add-task')
+const textarea = document.querySelector('.app__form-textarea')
+
+const tarefas = []
+
+btnAdicionarTarefa.addEventListener('click', () => {
+    formAdicionarTarefa.classList.toggle('hidden')
+})
+
+formAdicionarTarefa.addEventListener('submit', (evento) => {
+    evento.preventDefault();
+    const tarefa = {
+        descricao: textarea.value
+    }
+    tarefas.push(tarefa)
+    localStorage.setItem('tarefas', JSON.stringify(tarefas))
+})
