ac15a4f47fd948022e62c9f8d1706a24502e040a
Gustavo Silva <gustacar2008@gmail.com>
2024-08-15 20:16:39 -0300

    aula-7

--- script.js
+++ script.js
@@ -11,10 +11,10 @@ const iniciarOuPausarBt = document.querySelector('#start-pause span')
 const iniciarOuPausarBtIcone = document.querySelector(".app__card-primary-butto-icon") 
 const tempoNaTela = document.querySelector('#timer')
 
-const musica = new Audio('/sons/luna-rise-part-one.mp3')
-const audioPlay = new Audio('/sons/play.wav');
-const audioPausa = new Audio('/sons/pause.mp3');
-const audioTempoFinalizado = new Audio('./sons/beep.mp3')
+const musica = new Audio('sons/luna-rise-part-one.mp3')
+const audioPlay = new Audio('sons/play.wav');
+const audioPausa = new Audio('sons/pause.mp3');
+const audioTempoFinalizado = new Audio('.sons/beep.mp3')
 
 let tempoDecorridoEmSegundos = 1500
 let intervaloId = null
